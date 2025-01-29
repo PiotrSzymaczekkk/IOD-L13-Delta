27cfe5d71f0b41524196c4c485473d95558b436b
PiotrSzymaczekkk <pitrasekszymaczek@gmail.com>
2025-01-29 17:52:17 +0100

    Delete target directory

--- target/javadoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"pl.put.poznan.transformer"},{"l":"pl.put.poznan.transformer.logic"},{"l":"pl.put.poznan.transformer.rest"}]
